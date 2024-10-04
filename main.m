54d96a8b2c9f7f6e43aeabd723100a2550d69db5
JohnCimm <129312275+JohnCimm@users.noreply.github.com>
2024-10-04 15:39:23 -0600

    updates I made

--- main.m
+++ main.m
@@ -10,24 +10,30 @@ reply = input('Do you want more? Y/N [Y]:','s');
 if isempty(reply)
     reply = 'N';
 end
+
+% CONSTANT VARIABLES $$$$$$$$$$$$$$$$$$$$
+
 amp=1;%mA
 
 
 pw=200;%us
 
 freq=30;%Hz
+a_array = shuffle(linspace(2,6,20))
+f_array = shuffle(linspace(50 , 100,20))
+pw_array = linspace(200,200,20)
+% all 3 trials
 for i = 1:3 
     
     %s.stim([amp,pw,freq,1])
-    a_array = randi([2 6],20,1)
-    f_array = randi([50 100],20,1)
-    pw_array = linspace(200,200,20)
+    
     j = 1;
     while reply == 'Y' 
-        j1 = random
-        s.stim([a_array(j),pw_array(j),f_array(i),1])
+       [a,f] = main_switch(i,j,f_array,a_array)
+
+        s.stim([a,pw,f,1])
         
-        X = sprintf('Random Amplitude: %d mA, Random Frequency: %d Hz',a_array(j),f_array(j) );
+        %X = sprintf('Random Amplitude: %d mA, Random Frequency: %d Hz',a_array(j),f_array(j) );
         disp(X)
         reply = input('Do you want more? Y/N [Y]:','s');
         if isempty(reply)
